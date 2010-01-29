63a0deba2ab68081dea210f7cb4de7e5278ccd0e
Andrii Kasian <to.kandy@gmail.com>
2010-01-29 18:29:38 +0200

    Ref

--- System/View/Phptal.php
+++ System/View/Phptal.php
@@ -1,13 +1,19 @@
 <?php
 require_once 'PHPTAL.php';
 
-class System_View_Phptal extends System_View_Simple
+class System_View_Phptal extends Zend_View_Abstract
 {
 	/**
 	 * Engine
 	 * @var PHPTAL
 	 */
 	protected $_engine;
+
+    /**
+	 * Values starage
+	 * @var array
+	 */
+	protected $_vars = array();
 
 	public function __construct($config = array()) {
 		parent::__construct($config);
@@ -16,6 +22,65 @@ class System_View_Phptal extends System_View_Simple
 		}
 	}
 
+	/**
+	 * @see Zend_View_Interface::__isset
+	 */
+	public function  __set($name,  $value) {
+		$this->_vars[$name] = $value;
+	}
+    
+	/**
+	 * @see Zend_View_Interface::__get
+	 */
+	public function __get($key) {
+		if (isset($this->_vars[$key])) {
+			return $this->_vars[$key];
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * @see Zend_View_Interface::__isset
+	 */
+	public function __isset($key) {
+		return isset($this->_vars[$key]);
+	}
+
+	/**
+	 * @see Zend_View_Interface::__unset
+	 */
+	public function __unset($key) {
+		unset($this->_vars[$key]);
+	}
+
+	/**
+	 * @see Zend_View_Interface::assign
+	 */
+	public function assign($spec, $value = null) {
+		if (is_array($spec)) {
+			foreach ($spec as $k => $v) {
+				$this->__set($k, $v);
+			}
+		} else {
+            $this->__set($spec, $value);
+		}
+	}
+
+	/**
+	 * @see Zend_View_Abstract::getVars
+	 */
+	public function getVars() {
+		return $this->_vars;
+	}
+
+	/**
+	 * @see Zend_View_Interface::clearVars
+	 */
+	public function clearVars() {
+		$this->_vars = array();
+	}
+    
 	/**
 	 * @see Zend_View_Abstract::getEngine
 	 * @return PHPTAL
@@ -27,48 +92,15 @@ class System_View_Phptal extends System_View_Simple
 		return $this->_engine;
 	}
 
-	public function  __set($name,  $value) {
-		$this->getEngine()->set($name,  $value);
-	}
-	
     /**
-     * Finds a view script from the available directories.
-     *
-     * @param $name string The base name of the script.
-     * @return void
-     */
-    protected function _script($name)
-    {
-        if ($this->isLfiProtectionOn() && preg_match('#\.\.[\\\/]#', $name)) {
-            require_once 'Zend/View/Exception.php';
-            throw new Zend_View_Exception('Requested scripts may not include parent directory traversal ("../", "..\\" notation)');
-        }
-
-		$scriptPaths = $this->getScriptPaths();
-
-        if (0 == count($scriptPaths)) {
-            require_once 'Zend/View/Exception.php';
-            throw new Zend_View_Exception('no view script directory set; unable to determine location for view script',
-                $this);
-        }
-
-        foreach ($scriptPaths as $dir) {
-            if (is_readable($dir . $name)) {
-                return $dir . $name;
-            }
-        }
-
-        require_once 'Zend/View/Exception.php';
-        $message = "script '$name' not found in path ("
-                 . implode(PATH_SEPARATOR, $scriptPaths)
-                 . ")";
-        throw new Zend_View_Exception($message, $this);
-    }
-		/**
 	 * @see Zend_View_Abstract::_run
 	 */
 	protected function _run() {
 		$engine = $this->getEngine();
+        foreach ($this->_vars as $key => $value) {
+            $this->getEngine()->set($key,  $value);    
+        }
+        $this->getEngine()->set('this', $this);
 		$engine->setTemplate(func_get_arg(0));
 		return $engine->echoExecute();
 	}
